2ce2412bd697c27d1c1bdcf07c5a4bee94e816b8
Michal Ziemianek <mziemianek3@gmail.com>
2024-11-10 00:03:40 +0100

    no internal server error, but db disctonnected

--- src/api/main.py
+++ src/api/main.py
@@ -1,15 +1,38 @@
-from typing import Union
+import mysql.connector
+from fastapi import FastAPI, HTTPException
+import logging
+import os
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-from fastapi import FastAPI
+# Load environment variables
 
 app = FastAPI()
 
+def check_mysql_connection():
+    try:
+        cnx = mysql.connector.connect(
+            user=os.environ.get("MYSQL_USER"),
+            password=os.environ.get("MYSQL_PASSWORD"),
+            host=os.environ.get("MYSQL_HOST"),
+            database=os.environ.get("MYSQL_DATABASE")
+        )
+        if cnx.is_connected():
+            cnx.close()
+            return True
+    except mysql.connector.Error as e:
+        logger.error(f"Error connecting to MySQL: {e}")
+        return False
+    return False
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/db_check")
+def check_db():
+    if check_mysql_connection():
+        return {"status": "connected"}
+    else:
+        return {"status": "disconnected"}
